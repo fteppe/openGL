b14bb8e5acc8e89a4f59102079ac4b433a267b64
florent Teppe <teppe.florent@hotmail.fr>
2018-03-11 16:04:21 +0100

    fixed a weird bug I'm not even sure what was wrong.

--- Octree2/main.cpp
+++ Octree2/main.cpp
@@ -1,14 +1,10 @@
 // Octree2.cpp : Defines the entry point for the console application.
 //
-//#include "stdafx.h"
-
-#include <iostream>
-
-
+#include "stdafx.h"
 #include "WindowBuilder.h"
 
 
-void stuffs();
+class WindowBuilder;
 
 int main(int argc, char **argv)
 {
