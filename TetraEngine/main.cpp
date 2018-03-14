6ba3bb05e79273dc7adaf0a356444304aacdac1b
florent Teppe <teppe.florent@hotmail.fr>
2018-03-14 21:41:21 +0100

    Changed the libraries so They can work in the new project. I want to link all the libraries together so it can easely be used later

--- TetraEngine/main.cpp
+++ TetraEngine/main.cpp
@@ -1,10 +1,17 @@
 #include "TetraEngine.h"
 #include <QtWidgets/QApplication>
-
+#include <SceneLoader.h>
+#include <Scene.h>
+#include <Camera.h>
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	
+	tetraRender::Camera cam;
+	cam.getPos();
+
 	TetraEngine w;
 	w.show();
 	return a.exec();
+	
 }
