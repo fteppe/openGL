cd5af1b17bddaf96c46b06dea93a0df1cc4207d4
fteppe <florent.teppe@soprasteria.com>
2018-05-17 14:24:08 +0200

    Removed obsolete code; More work done towards getting functional shadows back in.

--- TetraRenderLib/Scene.cpp
+++ TetraRenderLib/Scene.cpp
@@ -26,60 +26,7 @@ tetraRender::Scene::Scene(Camera cam)
 	this->shadowProjection = this->cam;
 	shadowProjection.setPosition(glm::vec3(-2, 1, 2));
 	shadowProjection.setProjectionOrtho(2, 2, 1, 10);
-
-	std::shared_ptr<Texture> normalsBuffer(new Texture);
-	std::shared_ptr<Texture> depthBuffer(new Texture);
-	std::shared_ptr<Texture> spec(new Texture);
-	std::shared_ptr<Texture> fragPos(new Texture);
-	textures["color"] = std::shared_ptr<Texture>(new Texture);
-	textures["normals"] = normalsBuffer;
-	textures["depth"] = depthBuffer;
-	textures["specularity"] = spec;
-	textures["fragPos"] = fragPos;
-	textures["shadowDistance"] = std::shared_ptr<Texture>(new Texture);
-
 	clock.restart();
-	FrameBuffer * frame = new FrameBuffer;
-	frame->setHDR(true);
-	//frame->renderToScreen();
-
-	//ShadowMap frameBuffer
-	textures["shadowMap"] = std::shared_ptr<Texture>(new Texture);
-	frame->setDepthTexture(textures["shadowMap"]);
-	//shadow Passes
-	renderPasses.push_back(new RenderPass());
-	RenderPass * pass = renderPasses.back();
-	pass->setRenderOutput(frame);
-	pass->setRenderTagsIncluded({ WORLD_OBJECT });
-	pass->setCamera(&this->shadowProjection);
-	//We create a specific material that is very simple to render everything through it.
-	shaders["transform"] = std::shared_ptr<Shader>( new Shader("transform.ver", "transform.frag"));
-	materials["shadowMapMat"] = std::shared_ptr<Material>(new Material(shaders["transform"]));
-	pass->setMat(materials["shadowMapMat"]);
-
-	
-	//Colors renderPass.
-	frame = new FrameBuffer;
-	frame->addColorOutputTexture(textures["color"]);
-	frame->setDepthTexture(depthBuffer);
-	frame->addColorOutputTexture(normalsBuffer);
-	frame->addColorOutputTexture(spec);
-	frame->addColorOutputTexture(fragPos);
-	frame->addColorOutputTexture(textures["shadowDistance"]);
-
-	//We set the frame as the renderPass we want for the colors.
-	renderPasses.push_back(new RenderPass());
-	pass = renderPasses.back();
-	pass->setRenderOutput(frame);
-	pass->setRenderTagsIncluded({ WORLD_OBJECT });
-	pass->setCamera(&this->cam);
-
-	
-	//the second render pass we don't set a frameBuffer so it will render to the screen.
-	renderPasses.push_back(new RenderPass());
-	pass = renderPasses.back();
-	pass->setRenderTagsIncluded({ POST_PROCESS });
-	pass->setCamera(&this->cam);
 }
 
 
@@ -102,10 +49,7 @@ void tetraRender::Scene::animate(sf::Clock elapsed)
 	{
 		delete obj;
 	}
-	for (RenderPass* pass : renderPasses)
-	{
-		delete pass;
-	}
+
 }
 
 void tetraRender::Scene::setCamera(Camera camera) 
@@ -115,17 +59,7 @@ void tetraRender::Scene::setCamera(Camera camera)
 
 void tetraRender::Scene::renderScene()
 {
-	//It is very simple for now, but I don't believe this is how it should be done. With a hard coded pipeline
-	//Or a way to define specific steps I give myself the power to do more stuffs with stencil buffers and other.
-	//for (auto pass : renderPasses)
-	//{
- //		pass->renderScene(*this);
-	//	//textures["depth"]->readData();
-	//}
-	//renderPasses[0]->renderScene(*this);
-	//renderPasses[1]->renderScene(*this);
 	renderPipeLine->renderScene(*this);
-
 }
 
 void tetraRender::Scene::load(std::string scene)
@@ -270,40 +204,8 @@ void tetraRender::Scene::makeSkyBox()
 	gameObjects.push_back(sky);
 }
 
-void tetraRender::Scene::setupPostProcessing()
+void tetraRender::Scene::updateShadowMaps()
 {
-	//All this part is very exeprimental and hard coded weirdly which I would like to change in the future.
 
-	//for the post processing we need a flat triangle
-	std::vector<std::vector<int>> faces = { {0,1,2} };
-	std::vector<glm::vec3> points = { glm::vec3(-1,-1,0), glm::vec3(3,-1,0),glm::vec3(-1,3,0) };
-	Mesh * screen = new Mesh(points, faces);
-	//the UVs of our screen, the way it is set up we should have the sides of the screen aligned with the side of the square in the triangle.
-	std::vector<glm::vec3> uvs = { glm::vec3(0,0,0), glm::vec3(2,0,0), glm::vec3(0,2,0) };
-	screen->setUVs(uvs);
-	//we give it a name so we can more easely follow it.
-	screen->setFilePath(std::pair<std::string, std::string>("hard", "screen"));
-	std::shared_ptr<Mesh> vbo_ptr(screen);
-	Solid* screenObj = new Solid(vbo_ptr);
-	//models["hard"]["screen"] = vbo_ptr;
-	//this triangle has a different shader than usual.
-
-	Shader* shader = new ShaderPostProcess({ "postProcess.ver" },  { "defferedShading.frag" });
-	std::shared_ptr<Shader> shader_ptr(shader);
-	Material* mat = new Material(shader_ptr);
-	std::shared_ptr<Material> postProcessMat(mat);
-	postProcessMat->setChannel(textures["color"], "color");
-	postProcessMat->setChannel(textures["normals"], "normals");
-	postProcessMat->setChannel(textures["depth"], "depth");
-	postProcessMat->setChannel(textures["specularity"], "specularity");
-	postProcessMat->setChannel(textures["fragPos"], "fragPos");
-	postProcessMat->setChannel(textures["shadowDistance"], "shadowDistance");
-	postProcessMat->setChannel(textures["shadowMap"], "shadowMap");
-
-	screenObj->setMaterial(postProcessMat);
-	screenObj->addTag(POST_PROCESS);
-	//We add all these newly created elements to the scene;
-	shaders["postProcess"] = shader_ptr;
-	materials["postProcess"] = postProcessMat;
-	gameObjects.push_back(screenObj);	
 }
+
