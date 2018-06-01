3b360c2dd2d8b475503593d2cbb127e235f27463
fteppe <florent.teppe@soprasteria.com>
2018-06-01 14:31:05 +0200

    SDL is now the library used to render the window. I can now use Imgui to quickly build interfaces.

--- TetraVisu/WindowBuilder.cpp
+++ TetraVisu/WindowBuilder.cpp
@@ -37,7 +37,7 @@ WindowBuilder::WindowBuilder()
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 	SDL_DisplayMode current;
 	SDL_GetCurrentDisplayMode(0, &current);
-	window = SDL_CreateWindow("ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	window = SDL_CreateWindow("ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 	gl_context = SDL_GL_CreateContext(window);
 	SDL_GL_SetSwapInterval(1); // Enable vsync
 
@@ -46,7 +46,14 @@ WindowBuilder::WindowBuilder()
 	glEnable(GL_DEPTH_TEST);
 	glCullFace(GL_BACK);
 
+	// Setup Dear ImGui binding
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+	ImGui_ImplSdlGL3_Init(window);
 
+	// Setup style
 
 	
 	//apparently an old implementation bug tends to raise an error on startup. We call geterror to remove it.
@@ -64,8 +71,8 @@ WindowBuilder::WindowBuilder(std::string sceneFile) : WindowBuilder()
 {
 	tetraRender::Camera cam(600.0f, 800.0f, 0.75f);
 	cam.setUp(glm::vec3(0, 0, 0));
-	//scene = std::shared_ptr<tetraRender::Scene>(new tetraRender::Scene(cam));
-	//scene->load(sceneFile);
+	scene = std::shared_ptr<tetraRender::Scene>(new tetraRender::Scene(cam));
+	scene->load(sceneFile);
 
 }
 
@@ -97,49 +104,15 @@ void WindowBuilder::draw()
 			if (event.type == SDL_QUIT)
 				done = true;
 		}
+		scene->renderScene();
 		ImGui_ImplSdlGL3_NewFrame(window);
 
-		// 1. Show a simple window.
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-		{
-			static float f = 0.0f;
-			static int counter = 0;
-			ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
-
-		// 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
-
-		// 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-		if (show_demo_window)
-		{
-			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-			ImGui::ShowDemoWindow(&show_demo_window);
-		}
+		ImGui::Begin("ShaderEditor");
+		ImGui::Text("some small text");
+		ImGui::Button("ok");
 
 		// Rendering
 		glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
 		ImGui::Render();
 		ImGui_ImplSdlGL3_RenderDrawData(ImGui::GetDrawData());
 		SDL_GL_SwapWindow(window);
